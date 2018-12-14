097aa4f8a65472771a0a860ae7b7279ab795443e
Wildan Kurniadi <danprogramming10@gmail.com>
2018-12-15 00:42:16 +0700

    #01 Create our first Rectangle Container using stateless widget
    create rectangle using stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,28 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+
+//because we wouldn't want to write whole of our code in the main function, so we separate it into another function
+//in dart we can have 'top-level functions' this means function can be called outside of classes
+//Here we create one that returns helloRectangleContainer widgets,
+//chances are We're not going to have an app that is entirely hard-coded with widgets that never rebuild
+//but this is what our helloRectangleContainer function currently return
+//we can use a stateless widgets
+Widget helloRectangleContainer(){
+  return Container(
+    color: Colors.lightBlue,
+  );
+}
+
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
+        appBar : AppBar(
+          title: Text("Hello Rectangle"),
         ),
-        body: HelloRectangle(),
-      ),
-    ),
+        body: helloRectangleContainer(),
+      )
+    )
   );
-}
-
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
 }
